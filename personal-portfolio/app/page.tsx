7d5fb805585a3990e8fb35262074c4a44df1b135
amiraTech <rivercrayon@gmail.com>
2025-08-31 21:43:42 +0100

    Set up project local dev environment

--- personal-portfolio/app/page.tsx
+++ personal-portfolio/app/page.tsx
@@ -12,6 +12,12 @@ export default function Home() {
           height={38}
           priority
         />
+
+        <h1>Amira Portfolio, coming soon!</h1>
+
+
+
+
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
